153fb54f5d77a6ef762884f8efbb2022cfe5d8b1
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-02-05 13:54:58 -0800

    Fix

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,14 +1,12 @@
-import { Avatar } from "flowbite-react";
-
 /**
- * https://flowbite-react.com/docs/components/avatar#default-avatar
- * https://flowbite-react.com/docs/components/avatar#sizes
- * https://flowbite-react.com/docs/components/avatar#avatar-with-border
+ * https://flowbite.com/docs/components/avatar/#bordered
+ * https://flowbite.com/docs/components/avatar/#sizes
+ * https://flowbite.com/docs/components/avatar/#bordered
  */
 export default function ProfilePicture() {
   return (
     <div className="flex flex-wrap gap-2">
-      <Avatar img="/profile-pixel-art-upscaled.png" rounded size="xl" bordered />
+      <img className="rounded-full ring-3 ring-gray-300 dark:ring-gray-500 w-36 h-36" src="/profile-pixel-art-upscaled.png" alt="Extra large avatar"></img>
     </div>
   );
 }
